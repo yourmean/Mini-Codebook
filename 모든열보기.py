bbc78b30184a6bae53a8d79db90e626972218c74
Yumin Lee <55529646+yourmean@users.noreply.github.com>
2022-11-30 15:01:46 +0900

    221130

---
+++ 모든열보기.py
@@ -0,0 +1,10 @@
+# all columns
+pd.set_option('display.max_columns', None)
+
+# all rows
+pd.set_option('display.max_rows', None)
+
+
+# 옵션 원래대로
+pd.options.display.max_rows = 60
+pd.options.display.max_columns = 20
